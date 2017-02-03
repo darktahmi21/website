08c3891d38883f4b4082b0f9edf1bbe9d78e1641
Tahmid <darktahmi21@gmail.com>
2017-02-03 23:16:39 +0600

    Add files via upload

---
+++ Resources/js/script.js
@@ -0,0 +1,303 @@
+$(document).ready(function() {
+    
+    /* for hover function */
+    $(".js--fig-1").mouseover(function(){
+         $(".project-1").css("display", "block");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+    $(".js--fig-2").mouseover(function(){
+         $(".project-1").css("display", "none");
+         $(".project-2").css("display", "block");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+        
+     });
+    
+    $(".js--fig-3").mouseover(function(){
+          $(".project-1").css("display", "none");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "block");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+    $(".js--fig-4").mouseover(function(){
+         $(".project-1").css("display", "none");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "block");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+    $(".js--fig-5").mouseover(function(){
+          $(".project-1").css("display", "none");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "block");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+    $(".js--fig-6").mouseover(function(){
+          $(".project-1").css("display", "none");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "block");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+    $(".js--fig-7").mouseover(function(){
+          $(".project-1").css("display", "none");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "block");
+         $(".project-8").css("display", "none");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+    $(".js--fig-8").mouseover(function(){
+          $(".project-1").css("display", "none");
+         $(".project-2").css("display", "none");
+         $(".project-3").css("display", "none");
+         $(".project-4").css("display", "none");
+         $(".project-5").css("display", "none");
+         $(".project-6").css("display", "none");
+         $(".project-7").css("display", "none");
+         $(".project-8").css("display", "block");
+         $(".initial-project-text").css("display", "none");
+     });
+    
+        /* For contacts hover feature */
+
+    
+     $(".icon-1").mouseover(function(){
+         $(".contact-1").css("display", "block");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+         $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+    $(".icon-2").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "block");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+        $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+    $(".icon-3").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "block");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+        $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+     
+    $(".icon-4").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "block");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+        $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+     
+    $(".icon-5").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "block");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+        $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+     
+    $(".icon-6").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "block");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+        $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+     
+    $(".icon-7").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "block");
+         $(".contact-8").css("display", "none");
+         $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+     
+    $(".icon-8").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "block");
+         $(".contact-9").css("display", "none");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+     
+    $(".icon-9").mouseover(function(){
+         $(".contact-1").css("display", "none");
+         $(".contact-2").css("display", "none");
+         $(".contact-3").css("display", "none");
+         $(".contact-4").css("display", "none");
+         $(".contact-5").css("display", "none");
+         $(".contact-6").css("display", "none");
+         $(".contact-7").css("display", "none");
+         $(".contact-8").css("display", "none");
+         $(".contact-9").css("display", "block");
+         $(".initial-contact-text").css("display", "none");
+     });
+    
+    
+ 
+    
+    
+    
+    
+
+    
+    
+    /* For the sticky navigation feature */
+    
+    $('.js--section-interests').waypoint(function(direction){
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+            
+        }  
+    }, {
+        offset: '60px;'
+    });
+
+    /* Scroll on buttons */
+    
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-interests').offset().top}, 1000);
+    });
+
+
+    $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+    
+    /* Aninations on scroll */
+    
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');        
+    }, {
+        offset: '50%'
+    })
+
+/* mobile nav */
+    
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    })
+
+    
+    
+    
+    
+});
